8a79ae0f3be0b19df8d7a3471bcb496c153a0b96
Oz Shwartz <Ozshwartz@gmail.com>
2024-12-14 23:04:31 +0200

    new runner

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "Final-Project-technion-2468"
+    bucket         = "final-project-technion-2468"
     key            = "terraform/state/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
